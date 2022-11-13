cc3f49ae4b8ca92a60e924adedfea08eb6b95183
GrimsCoder <magrimwood2011@gmail.com>
2022-11-13 10:42:02 +0200

    Update DashboardTable.js

--- src/components/tables/DashboardTable.js
+++ src/components/tables/DashboardTable.js
@@ -78,7 +78,7 @@ function DashboardTable() {
       <table>
         <thead className=" tester sticky -webkit-sticky  top-0 bg-white z-30">
           <tr>
-            <th className=" tester sticky -webkit-sticky   left-0 bg-white z-30 flex items-center justify-between">
+            <th className=" tester block sticky -webkit-sticky   left-0 bg-white z-30 flex items-center justify-between">
               <button
                 type="button"
                 onClick={() => requestSort("token")}
@@ -281,7 +281,7 @@ function DashboardTable() {
               className="hover:bg-slate-50 hover:translate-y-px z-10"
             >
               {/* <Link to={`/scores/${asset.token}`}> */}
-              <td className="tester flex justify-start items-center sticky -webkit-sticky  left-0 bg-white  hover:bg-slate-50 z-20">
+              <td className="tester block flex justify-start items-center sticky -webkit-sticky  left-0 bg-white  hover:bg-slate-50 z-20">
                 <img
                   src={asset.logo}
                   className="w-8 mr-3 rounded-full"
